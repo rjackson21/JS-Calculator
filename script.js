e5b48c5226a6a1454a5d1f95d076b0b1fd6d1afd
Regina Jackson <rjackson156@gmail.com>
2022-04-05 21:45:34 -0400

    storing keys as variables for future operation functions

--- script.js
+++ script.js
@@ -27,24 +27,36 @@ keys.addEventListener('click', function(e) {
     const key = e.target
     const keyContent = key.textContent
 
-
+    if (key.classList.contains('equal')) {
+        const question = displayedNum
+       // operate(num1, num2, operator)
+    }
+    else {
         if (displayedNum === '0') {
             display.textContent = keyContent
             displayedNum = display.textContent
+            const num1 = displayedNum
+            console.log(num1)
         }
         else if (key.classList.contains('clear')){
             display.textContent = '0'
             displayedNum = display.textContent
         }
-        else if (key.classList.contains('equal')) {
-            const question = displayedNum
-            console.log(displayedNum)
-        }
-        else {
+        else if (!(key.classList.contains('operator'))){
             display.textContent = displayedNum + keyContent
             displayedNum = display.textContent
+            num1 = displayedNum
+            console.log(num1)
         }
-
+        else if (key.classList.contains('operator')){
+            key.classList.add("change")
+            num1 = displayedNum
+            displayedNum = display.textContent
+            const operation = keyContent
+            console.log(num1)
+            console.log(operation)
+        }
+    }
     
 })
 
